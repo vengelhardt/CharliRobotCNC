76b0d8f2ebaf7dc05fe723859430f897a600ba07
Kit Ex Machina <machinekit@beaglebone.localdomain>
2018-07-04 19:49:42 +0000

    Cape OK et Homing ok

--- init_CNC.sh
+++ init_CNC.sh
@@ -12,7 +12,7 @@ fi
 
 # Installation du pilote écran 7 pouces si pas installé
 export LCD_FILE=/usr/share/gmoccapy_lcd7
-if [ ! -f "$LCD_FILE" ]; then
+if [ ! -d "$LCD_FILE" ]; then
         echo "Installation de l ecran"
 	git clone https://github.com/vichente1/gmoccapy_lcd7.git
 	cd gmoccapy_lcd7/	
@@ -43,18 +43,30 @@ echo 30 | sudo tee $GPIO/export		# P9.11	xlim in
 echo 31 | sudo tee $GPIO/export		# P9.13 ylim in
 echo 23 | sudo tee $GPIO/export         # P8.13 zlim in
 
-echo 67 | sudo tee $GPIO/export         # P8.04	xok out
-echo 68 | sudo tee $GPIO/export         # P8.06 yok out
-echo 44 | sudo tee $GPIO/export         # P8.08 zok out
+echo 4  | sudo tee $GPIO/export         # P9.18	xok out
+echo 5  | sudo tee $GPIO/export         # P9.17 yok out
+echo 26 | sudo tee $GPIO/export         # P8.14 zok out
+
+echo 14 | sudo tee $GPIO/export         # P9.26	xen out
+echo 15 | sudo tee $GPIO/export         # P9.24 yen out
+echo 68 | sudo tee $GPIO/export         # P8.10 yok out
+
+echo 47 | sudo tee $GPIO/export         # P8.15 RELAY
 
 echo Definition de la direction
 echo "in" | sudo tee $GPIO/gpio30/direction
 echo "in" | sudo tee $GPIO/gpio31/direction
 echo "in" | sudo tee $GPIO/gpio23/direction
 
-echo "out" | sudo tee $GPIO/gpio67/direction
+echo "out" | sudo tee $GPIO/gpio4/direction
+echo "out" | sudo tee $GPIO/gpio5/direction
+echo "out" | sudo tee $GPIO/gpio26/direction
+
+echo "out" | sudo tee $GPIO/gpio47/direction
+
+echo "out" | sudo tee $GPIO/gpio14/direction
+echo "out" | sudo tee $GPIO/gpio15/direction
 echo "out" | sudo tee $GPIO/gpio68/direction
-echo "out" | sudo tee $GPIO/gpio44/direction
 
 exit 0
 
